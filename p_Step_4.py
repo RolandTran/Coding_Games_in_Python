bbff571bb1ea95d3a3fc2fd1375a48aab5ade196
RolandTran <rolandmtran@gmail.com>
2024-07-14 16:14:24 -0700

    Add files via upload

---
+++ p_Step_4.py
@@ -0,0 +1,26 @@
+# start of notes;
+# p = prototype;
+# Step 4: set the apple iamge at (300,200) when player clicks mouse
+# prints "Good shott!" and "You missed! Game over!" when miss click
+
+import pgzrun
+
+
+WIDTH = 800
+HEIGHT = 600
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.pos = (300, 200)
+    apple.draw()
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot! You hit the apple!")
+    else:
+        print("You missed! Game over!")
+        quit() # exit games when miss click
+
+pgzrun.go()
---
+++ p_Step_4a.py
@@ -0,0 +1,26 @@
+# start of notes;
+# p = prototype;
+# Step 4a: set the apple iamge at (300,200) when player clicks mouse
+# prints "Good shott!" and "You missed! Game over!" when miss click; variation
+
+import pgzrun
+import sys
+
+WIDTH = 800
+HEIGHT = 600
+
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.pos = (300, 200)
+    apple.draw()
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("Good shot! You hit the apple!")
+    else:
+        print("You missed! Game over!")
+        sys.exit() # exit games when miss click; also can use quit()
+
+pgzrun.go()
