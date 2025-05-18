f84286d86f0556237401aae1f071eb9ed7682b9e
RolandTran <rolandmtran@gmail.com>
2025-05-17 20:55:47 -0700

    Add files via upload

---
+++ InititalsAsterisk.py
@@ -0,0 +1,14 @@
+# Printing the initials RMT using asterisks
+def print_initials():
+    print("*******    **          **   ********")
+    print("**    **   ****      ****      **   ")
+    print("**   **    ** **    ** **      **   ")
+    print("**  **     **  **   ** **      **   ")
+    print("*****      **  **   ** **      **   ")
+    print("**   **    **   ** **  **      **   ")
+    print("**    **   **   ** **  **      **   ")
+    print("**     **  **    **    **      **   ")
+    print("**     **  **    **    **      **   ")
+
+# Call the function to print the initials
+print_initials()
